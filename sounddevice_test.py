9260f024e59a66ca5a10dcd560bf9508c2e7433c
Alexis THIBAULT <alexis.thibault@inria.fr>
2019-11-18 18:55:54 +0100

    Demo

--- sounddevice_test.py
+++ sounddevice_test.py
@@ -5,15 +5,21 @@ Created on Mon Nov 18 17:06:37 2019
 
 @author: alexis
 """
+import numpy as np
 import sounddevice as sd
+from periodfinder import PeriodFinder
 
-duration = 5.5  # seconds
+#duration = 5.5  # seconds
+
+pf = PeriodFinder()
 
 def callback(indata, outdata, frames, time, status):
     if status:
         print(repr(status))
-    print(len(indata), len(outdata))
-    outdata[:] = indata
+    mono = np.sum(indata, axis=1)
+    f = pf(mono)
+    print(f)
 
 with sd.Stream(channels=2, callback=callback):
-    sd.sleep(int(duration * 1000))
+    while True:
+        sd.sleep(100)
