0dd15f6a6e112fd45e81144fbb2b4f341746bff1
Alexis THIBAULT <alexis.thibault@inria.fr>
2019-12-23 16:12:10 +0100

    Add FM to OneNoteSynth

--- play_midi_file.py
+++ play_midi_file.py
@@ -6,6 +6,7 @@ Created on Sun Dec 15 21:37:38 2019
 """
 import mido
 import sounddevice as sd
+import time
 
 from synth import *
 
@@ -26,39 +27,39 @@ class MidiFilePlayer:
         self.msg_handler = msg_handler
         self.iterator = self.callback_iterator()
         self.timer = timer
-        
+
         self.finished = False
-    
+
     def callback_iterator(self):
         for msg in self.mid:
             self.last_note_ns = self.timer()
             while (self.timer() - self.last_note_ns) < msg.time:
                 yield
             self.msg_handler(msg)
-            
+
             elapsed =  (self.timer() - self.last_note_ns)
             err = elapsed - msg.time
-            if err > 0.01:
+            if err > 0.003:
                 print("Oops! I'm {:.3f}s late".format(err))
-                
-        self.finished = True
-            
+
     def callback(self):
         """Call this often. Checks if we should send any MidiMessage now."""
-        next(self.iterator)
-            
+        try:
+            next(self.iterator)
+        except StopIteration:
+            self.finished = True
 
 
 def play_midi_file(filename, synth):
-    
+
     player = MidiFilePlayer(filename,
                             msg_handler=synth.receive,
                             timer=synth.get_time)
-    
+
     def callback(indata, outdata, frames, time, status):
         outdata[:, 0] = synth.get_data(frames)
         player.callback()
-        
+
     with sd.Stream(samplerate=SAMPLERATE, channels=1, callback=callback, blocksize=128):
         while not player.finished:
             time.sleep(1)
@@ -69,5 +70,4 @@ if __name__ == '__main__':
     filename = 'Dejected_Groose_piano.mid'
     print("Playing", filename, "with ORGAN_SYNTH")
     play_midi_file(filename, ORGAN_SYNTH)
-    
-    
+
--- synth.py
+++ synth.py
@@ -6,6 +6,7 @@ Created on Sun Dec 15 16:45:23 2019
 """
 import collections
 import threading
+import abc
 
 import numpy as np
 
@@ -24,12 +25,12 @@ class EnvelopeGain:
         self._cur_level = 0.0
         self._released = False
         self._t = 0
-        
+
         self.is_alive = True
-    
+
     def get_gain(self, frames):
         """Get the next `frames` frames of the gain.
-        
+
         frames : int.
         returns : an array of length `frames`
         raises : NoteFinished
@@ -56,14 +57,14 @@ class EnvelopeGain:
         self._cur_level = result[-1]
         self._t = next_t
         return result
-        
-        
+
+
     def note_off(self):
         """Begin the 'release' phase of the note."""
         self._released = True
         self._release_volume = self._cur_level
         self._t = 0
-    
+
 
 DEFAULT_ENVELOPE = Envelope(1e-3, 1e-3, 1.0, 1e-3)
 TEST_ENVELOPE = Envelope(0.1, 0.2, 0.3, 0.4)
@@ -73,38 +74,74 @@ SAWTOOTH_WAVE = lambda x: 2*x-1
 TRIANGLE_WAVE = lambda x: 4*abs(x-1/2)-1
 SINE_WAVE = lambda x: np.sin(2*np.pi*x)
 SQUARE_WAVE = lambda x: 1.0*(x<1/2) - 1.0*(x>1/2)
+def offset_triangle(alpha=0.2):
+    return lambda x: (x<alpha)*(2*x/alpha - 1) \
+        + (x>alpha) * (1-2*(x-alpha)/(1-alpha))
+OFFSET_TRI_02 = offset_triangle(0.2)
+OFFSET_TRI_01 = offset_triangle(0.1)
 
 def freq_of_note(note):
     return 440 * 2**((note-69)/12)
 
 
-class OneNoteSynth:
-    
-    def __init__(self, frequency, volume, waveform, envelope):
+class Synth(abc.ABC):
+    @abc.abstractmethod
+    def get_data(self, frames):
+        """Get the next `frames` frames of data output by this synth.
+
+        Parameters
+        ----------
+        frames : int
+            Number of frames of data to read.
+
+        Returns
+        -------
+        data : np.array
+            Array of length `frames`."""
+
+
+class NullSynth(Synth):
+    def get_data(self, frames):
+        return np.zeros(frames)
+
+
+class OneNoteSynth(Synth):
+
+    def __init__(self, frequency, volume, waveform, envelope,
+                 fm=NullSynth(), fm_strength=1.0):
         self._envelope = EnvelopeGain(envelope)
         self._waveform = waveform
         self._volume = volume
         self._frequency = frequency
-        self._t = 0.0
+        self._fm = fm
+        self._fm_strength = fm_strength
+        self._t_norm = 0.0
         self.is_alive = True
-        
+
+    def set_fm_strength(self, fm_strength):
+        self._fm_strength = fm_strength
+
     def get_data(self, frames):
         data = self._create_wave_data(frames) * self._envelope.get_gain(frames)
         self.is_alive = self._envelope.is_alive
         return data * self._volume
-    
+
     def _create_wave_data(self, frames):
-        tt = self._t + np.arange(frames)/SAMPLERATE
-        tt_normalized = (tt * self._frequency) % 1
-        self._t += frames/SAMPLERATE
-        return self._waveform(tt_normalized)
-    
+#        tt = self._t + np.arange(frames)/SAMPLERATE
+#        tt_norm = (tt * self._frequency) % 1
+#        self._t += frames/SAMPLERATE
+        fm_data = self._fm_strength * self._fm.get_data(frames)
+        tt_norm = self._t_norm + np.cumsum(self._frequency * (1 + fm_data))/SAMPLERATE
+        tt_norm %= 1.0
+        self._t_norm = tt_norm[-1]
+        return self._waveform(tt_norm)
+
     def note_off(self):
         self._envelope.note_off()
-    
-    
-    
-class PolyphonicSynth:
+
+
+
+class PolyphonicSynth(Synth):
     def __init__(self, waveform, envelope, max_polyphony=10, gain=0.1):
         self._waveform, self._envelope = waveform, envelope
         self.note_synths = collections.OrderedDict()
@@ -112,9 +149,9 @@ class PolyphonicSynth:
         self.max_polyphony = max_polyphony
         self.gain = gain
         self._lock = threading.Lock()
-        
+
         self._t = 0.0
-        
+
     def receive(self, msg):
         if msg.type == 'note_on' and msg.velocity > 0:
             self.note_on(msg.note, msg.velocity)
@@ -122,25 +159,30 @@ class PolyphonicSynth:
             or msg.type == 'note_off':
             self.note_off(msg.note)
         self.bury_dead_notes()
-        
+
     def get_time(self):
+        """Get current time according to the number of samples sent.
+
+        Precision is of the order of the audio block size,
+        so frames/SAMPLERATE.
+        """
         return self._t
-    
+
     def note_on(self, note, velocity):
         # Don't let the same note play twice at the same time.
-        self.note_off(note) 
-        
+        self.note_off(note)
+
         # Don't let more than `max_polyphony` notes
         # play at the same time.
         if len(self.note_synths) >= self.max_polyphony:
             note_to_kill = next(iter(self.note_synths))
             self.note_off(note_to_kill)
-            
+
         note_synth = OneNoteSynth(freq_of_note(note), velocity/128,
                                   self._waveform, self._envelope)
         with self._lock:
             self.note_synths[note] = note_synth
-        
+
     def note_off(self, note):
         with self._lock:
             try:
@@ -149,14 +191,14 @@ class PolyphonicSynth:
                 self.note_synths_dying.append(prev_note)
             except KeyError:
                 pass
-            
+
     def get_data(self, frames):
         with self._lock:
             datas = [note_synth.get_data(frames) for note_synth in self.note_synths.values()] \
              + [note_synth.get_data(frames) for note_synth in self.note_synths_dying]
             self._t += frames/SAMPLERATE
             return np.sum(datas, axis=0) * self.gain
-    
+
     def bury_dead_notes(self):
         with self._lock:
             self.note_synths_dying = list(filter(
@@ -169,25 +211,25 @@ BASS_ENVELOPE = Envelope(1e-4, 0.1, 0.8, 1e-3)
 BASS_SYNTH = PolyphonicSynth(SAWTOOTH_WAVE, BASS_ENVELOPE, max_polyphony=1)
 
 ORGAN_ENVELOPE = Envelope(0.05, 0.5, 0.8, 0.2)
-ORGAN_SYNTH = PolyphonicSynth(TRIANGLE_WAVE, ORGAN_ENVELOPE)
+ORGAN_SYNTH = PolyphonicSynth(OFFSET_TRI_01, ORGAN_ENVELOPE)
 
 NO_ENVELOPE = Envelope(1e-3, 1e-3, 1.0, 1e-3)
 CHIPTUNE = PolyphonicSynth(SQUARE_WAVE, NO_ENVELOPE)
 
-if __name__ == '__main__':    
+
+if __name__ == '__main__':
     import sounddevice as sd
     import mido
     import time
-    
-    
+
+
     synth = ORGAN_SYNTH
-    
+
     def callback(indata, outdata, frames, time, status):
         outdata[:, 0] = synth.get_data(frames)
-        
+
     with sd.Stream(samplerate=SAMPLERATE, channels=1, callback=callback):
         with mido.open_input() as inport:
             inport.callback = synth.receive
             while not inport.closed:
                 time.sleep(0.1)
-    
